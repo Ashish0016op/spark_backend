55675fb34da949f8aa82ea5817588a14aca6e02b
Ashish0016op <raccoonop0016@gmail.com>
2024-05-18 17:23:12 +0530

    live

--- backend/app.js
+++ backend/app.js
@@ -3,6 +3,19 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const DB=process.env.MONGODB_URL;
+
+const cors = require('cors');
+const customerRoute = require('./routes/customer');
+const transactionRoute=require('./routes/TransactionHistory');
+app.use(cors());
+app.use(express.json());
+app.use(customerRoute);
+app.use(transactionRoute);
+
+app.get("/",(req,res)=>{
+    res.send("Hi, I am live");
+})
+
 mongoose.connect(DB,{
     useNewUrlParser:true,
     useUnifiedTopology:true,
@@ -16,12 +29,5 @@ const PORT=5000;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
 });
-const cors = require('cors');
-const customerRoute = require('./routes/customer');
-const transactionRoute=require('./routes/TransactionHistory');
-app.use(cors());
-app.use(express.json());
-app.use(customerRoute);
-app.use(transactionRoute);
 
 
